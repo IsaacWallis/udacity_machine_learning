54d32fd392a214d26e5e72fb581449733f0b5e08
IsaacWallis <ikewallis@gmail.com>
2017-04-28 17:29:11 -0600

    adds momentum to gradient descent

--- capstone/gradient_descent.py
+++ capstone/gradient_descent.py
@@ -11,7 +11,7 @@ if __name__ == "__main__":
     labels = seg_data["labels"]
     pixels = seg_data["pixels"]
 
-    label = 10
+    label = 12
     patch_indices = np.where(labels == label)
     patch_pixels = pixels[patch_indices]    
     indices_at_origin = (patch_indices[0] - np.min(patch_indices[0]),
@@ -19,10 +19,11 @@ if __name__ == "__main__":
 
     agent = GD_Searcher(patch_pixels, indices_at_origin)
     env = Env(pixels, agent)
+    print "environment size:", pixels.shape
 
     print "Starting"
     state = env.init_episode()
-    gamma = 100
+    gamma = 50.
     error = env.calculate_error(state)
 
     y_axis = []
@@ -31,19 +32,20 @@ if __name__ == "__main__":
     count = 0
     start = time.time()
 
-    for i in range(400):
+    momentum = 0.9
+    velocity = np.zeros(state.shape)
+    for i in range(1,1000):
         gradients = env.get_gradients(state)
-        scaled = gamma * gradients
-        state = state - scaled
+        velocity = (momentum * velocity) + (gamma  * gradients)
+        state = state - velocity
         state = env.check_state(state)            
         error_prime = env.calculate_error(state)
         improvement = error - error_prime
         error = error_prime
-
         y_axis.append(error)
         x_axis.append(count)
         count += 1
-        #print state, error, improvement
+
     print time.time() - start
     print "Done!"
     import matplotlib.pyplot as plt
