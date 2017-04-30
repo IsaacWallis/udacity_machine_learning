c3e7b0e8888ab8df33b96fd6e4cded04272cc999
IsaacWallis <ikewallis@gmail.com>
2017-04-30 15:45:36 -0600

    forgot to indlude gradient descent module in last commit

--- capstone/gradient_descent.py
+++ capstone/gradient_descent.py
@@ -8,10 +8,12 @@ class Env:
     """
     An environment that takes an image, distributes rewards, allows actions. Displayable.
     """
-    def __init__(self, image, agent):
+    def __init__(self, image, agent_pixels, agent_indices):
         self.image = image
-        self.agent = agent
-        
+        self.agent_pixels = agent_pixels
+        self.agent_indices = agent_indices
+        self.heatmap = np.zeros((env_pixels.shape[0] - np.max(agent_indices[0]),
+                                 env_pixels.shape[1] - np.max(agent_indices[1])))        
         # defines small step value for each element of the state
         self.epsilon = {
             0 : np.array([1, 0]),
@@ -21,10 +23,10 @@ class Env:
 
     def calculate_error(self, state):
         rounded =  np.rint(state).astype(np.int8)
-        x_indices = self.agent.indices[0] + rounded[0]
-        y_indices = self.agent.indices[1] + rounded[1]
+        x_indices = self.agent_indices[0] + rounded[0]
+        y_indices = self.agent_indices[1] + rounded[1]
         envPix = self.image[x_indices, y_indices, :] / 255.
-        agPix = self.agent.pixels / 255.
+        agPix = self.agent_pixels / 255.
         e = envPix - agPix
         e = e * e
         e = np.sum(e, axis = 1) / 3.
@@ -43,11 +45,11 @@ class Env:
             #print gradient_vector
         return gradient_vector
 
-    def init_episode(self):
+    def get_random_state(self):
         """
         :returns: a randomly selected state
         """
-        agent_x, agent_y = self.agent.indices
+        agent_x, agent_y = self.agent_indices
         minX = np.min(agent_x)
         minY = np.min(agent_y)
         maxX = np.max(agent_x)
@@ -58,7 +60,7 @@ class Env:
         ], dtype=np.float64)
 
     def check_state(self, state):
-        agent_x, agent_y = self.agent.indices
+        agent_x, agent_y = self.agent_indices
         minX = np.min(agent_x)
         minY = np.min(agent_y)
         maxX = np.max(agent_x)
@@ -72,136 +74,85 @@ class Env:
         if state[1] + maxY > self.image.shape[1]:
             state[1] = maxY
         return state
+
+    def start_agent(self, initState = None, gamma = 50., momentum = 0.9, maxT = 1000, plot = False):
         
-class GD_Searcher:
-    def __init__(self, pixels, indices):
-        self.pixels = pixels
-        self.indices = indices
-
-def gradient_descent_convergence_plot(env_pixels, patch_pixels, patch_indices):
-    agent = GD_Searcher(patch_pixels, indices_at_origin)
-    env = Env(env_pixels, agent)
-    print "environment size:", env_pixels.shape
-
-    print "Starting"
-    state = env.init_episode()
-    gamma = 50.
-    error = env.calculate_error(state)
-
-    y_axis = []
-    x_axis = []
-
-    count = 0
-    start = time.time()
-
-    momentum = 0.9
-    velocity = np.zeros(state.shape)
-    for i in range(1,1000):
-        gradients = env.get_gradients(state)
-        velocity = (momentum * velocity) + (gamma  * gradients)
-        state = state - velocity
-        state = env.check_state(state)            
-        error_prime = env.calculate_error(state)
-        improvement = error - error_prime
-        error = error_prime
-        y_axis.append(error)
-        x_axis.append(count)
-        count += 1
-
-    print time.time() - start
-    print "Done!"
-    import matplotlib.pyplot as plt
-    plt.plot(x_axis, y_axis)
-    plt.show()
-
-def grad_descent(env_pixels, patch_pixels, patch_indices):
-    agent = GD_Searcher(patch_pixels, indices_at_origin)
-    env = Env(env_pixels, agent)
-    print "environment size:", env_pixels.shape
-
-    print "Starting"
-    state = env.init_episode()
-    gamma = 50.
-    error = env.calculate_error(state)
-
-    count = 0
-    start = time.time()
-
-    momentum = 0.9
-    velocity = np.zeros(state.shape)
-    smallestError = sys.float_info.max
-    bestState = state
-    convergenceAtBest = 0
-    for i in range(1,1000):
-        gradients = env.get_gradients(state)
-        velocity = (momentum * velocity) + (gamma  * gradients)
-        state = state - velocity
-        state = env.check_state(state)            
-        error_prime = env.calculate_error(state)
-        if error_prime < smallestError:
-            smallestError = error_prime
-            bestState = state
-            convergenceAtBest = count
-
-        improvement = error - error_prime
-        error = error_prime        
-        count += 1
-    return bestState, smallestError, convergenceAtBest
-
-def grad_descent_heatmap(env_pixels, patch_pixels, patch_indices):
-    agent = GD_Searcher(patch_pixels, indices_at_origin)
-    env = Env(env_pixels, agent)
-    print "environment size:", env_pixels.shape
-
-    print "Starting"
-    state = env.init_episode()
-    gamma = 50.
-    error = env.calculate_error(state)
-
-    count = 0
-    start = time.time()
-
-    momentum = 0.9
-    velocity = np.zeros(state.shape)
-    smallestError = sys.float_info.max
-    bestState = state
-    convergenceAtBest = 0
-    x_ndcs = patch_indices[0]
-    y_ndcs = patch_indices[1]
-    heatmap = np.zeros((env_pixels.shape[0] - np.max(x_ndcs), env_pixels.shape[1] - np.max(y_ndcs)))
-    for i in range(1,1000):
-        gradients = env.get_gradients(state)
-        velocity = (momentum * velocity) + (gamma  * gradients)
-        state = state - velocity
-        state = env.check_state(state)            
-        error_prime = env.calculate_error(state)
-        state_ndx = np.round(state).astype(np.int8)
-        heatmap[state_ndx[0], state_ndx[1]] = error_prime
-        if error_prime < smallestError:
-            smallestError = error_prime
-            bestState = state
-            convergenceAtBest = count
+        state = initState
+        if initState is None:
+            state = self.get_random_state()
             
-        improvement = error - error_prime
-        error = error_prime        
-        count += 1
-    print bestState, smallestError, convergenceAtBest
-    return heatmap
+        error = self.calculate_error(state)
+
+        count = 0
+        start = time.time()
+
+        velocity = np.zeros(state.shape)
+        smallestError = sys.float_info.max
+        bestState = state
+        convergenceAtBest = 0
+        
+        error_axis = []
+        time_axis = []
+
+        for i in range(maxT):
+            gradients = self.get_gradients(state)
+            velocity = (momentum * velocity) + (gamma  * gradients)
+            state = state - velocity
+            state = self.check_state(state)            
+            error_prime = self.calculate_error(state)
+            state_ndx = np.round(state).astype(np.int8)
+            self.heatmap[state_ndx[0], state_ndx[1]] = error_prime
+            if plot:
+                error_axis.append(error_prime)
+                time_axis.append(count)
+            if error_prime < smallestError:
+                smallestError = error_prime
+                bestState = state
+                convergenceAtBest = count
+
+            improvement = error - error_prime
+            error = error_prime        
+            count += 1
+        if plot:    
+            import matplotlib.pyplot as plt
+            plt.plot(time_axis, error_axis)
+            plt.show()
+        return bestState, smallestError, convergenceAtBest        
+
+    def get_heatmap(self):
+        return self.heatmap
     
+def multi_gradient_descent(env_pixels, patch_pixels, patch_indices):
+    env = Env(env_pixels, patch_pixels, patch_indices)
+    initStateA = env.get_random_state()
+    initStateB = env.get_random_state()
+    stateA, rewardA, tA = env.start_agent(initState = initStateA, maxT = 50)
+    stateB, rewardB, tB = env.start_agent(initState = initStateB, maxT = 50)
+    for i in range(20):        
+        if rewardA < rewardB:
+            stateB, rewardB, tB = env.start_agent( maxT = 50)
+        elif rewardB < rewardA:
+            stateA, rewardA, tA = env.start_agent( maxT = 50)
+            
+    import patch_search
+    patch_search.plot_heatmap(env.get_heatmap())
 
 if __name__ == "__main__":
     img_name = 'butterfly'
     K = 150
     seg_data = image_segment.get_segmented_img(img_name, K)
     labels = seg_data["labels"]
-    pixels = seg_data["pixels"]
+    env_pixels = seg_data["pixels"]
 
     label = 12
     patch_indices = np.where(labels == label)
-    patch_pixels = pixels[patch_indices]    
+    patch_pixels = env_pixels[patch_indices]    
     indices_at_origin = (patch_indices[0] - np.min(patch_indices[0]),
                          patch_indices[1] - np.min(patch_indices[1]))
-    heatmap =  grad_descent_heatmap(pixels, patch_pixels, patch_indices)
-    import patch_search
-    patch_search.plot_heatmap(heatmap)
+    print multi_gradient_descent(env_pixels, patch_pixels, indices_at_origin)
+    #heatmap =  grad_descent_heatmap(env_pixels, patch_pixels, patch_indices)
+    #import patch_search
+    #patch_search.plot_heatmap(heatmap)
 
+    #max_ndx = np.unravel_index(np.argmax(heatmap), heatmap.shape)
+    #patch_search.display_patch(env_pixels, patch_pixels, patch_indices, max_ndx)
