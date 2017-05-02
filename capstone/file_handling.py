7076628631eee9c893ee0595d33e5366789c1091
IsaacWallis <ikewallis@gmail.com>
2017-05-02 16:03:38 -0600

    switching to pycharm

--- capstone/file_handling.py
+++ capstone/file_handling.py
@@ -1,9 +1,12 @@
 import os
 import pickle
+from scipy import ndimage
 
-segmented_dir = "./segmented/"
+segmented_dir = "./segmented"
 main_dir = "./progress"
 segment_file_name = "segment.pickle"
+image_dir = "./images"
+
 
 def get_project_directory(name):
     if not os.path.exists(main_dir):
@@ -12,6 +15,7 @@ def get_project_directory(name):
     if not os.path.exists(project_path):
         os.makedirs(project_path)
     return project_path    
+
 
 def project_exists(name):
     if not os.path.exists(main_dir):
@@ -23,24 +27,38 @@ def project_exists(name):
     if not os.path.exists(segment_file):
         return False
     return True
-    
+
+
 def write_segment_file(name, data):
     project_path = get_project_directory(name)
-    segfile_path = os.path.join(project_path, segment_file_name)
-    output = open(segfile_path, 'ab+')
+    segment_file_path = os.path.join(project_path, segment_file_name)
+    output = open(segment_file_path, 'ab+')
     pickle.dump(data, output)
     output.close()
 
+
+def remove_segment_file(name):
+    project_path = get_project_directory(name)
+    segment_file_path = os.path.join(project_path, segment_file_name)
+    os.remove(segment_file_path)
+
+
 def read_segment_file(name):
     project_path = get_project_directory(name)
-    segfile_path = os.path.join(project_path, "segment.pickle")
+    segment_file_path = os.path.join(project_path, "segment.pickle")
     if project_exists(name):
-        output = open(segfile_path, 'rb')
+        output = open(segment_file_path, 'rb')
         data = pickle.load(output)
         output.close()
         return data
     else:
         raise IOError("Can't read project, it doesn't exist.")
+
+
+def get_image(name):
+    path = os.path.join(image_dir, name + ".jpg")
+    img = ndimage.imread(path)
+    return img
 
 if __name__ == "__main__":
     pass
--- capstone/image_segment.py
+++ capstone/image_segment.py
@@ -25,10 +25,10 @@ def segment(img, K):
         X = np.reshape(img, (img.shape[0] * img.shape[1], img.shape[2]))
     elif len(img.shape) is 2:
         X = np.reshape(img, (img.shape[0] * img.shape[1], 1))
-    ward =  AgglomerativeClustering(n_clusters = K, linkage = 'ward',
-                                    connectivity = connectivity)
+    ward = AgglomerativeClustering(n_clusters=K, linkage='ward',
+                                   connectivity=connectivity)
     ward.fit(X)
-    labels = np.reshape(ward.labels_, (img.shape[0], img.shape[1] ))
+    labels = np.reshape(ward.labels_, (img.shape[0], img.shape[1]))
     print("Elapsed time: ", time.time() - t0)
     print("Number of pixels: ", labels.size)
     print("Number of clusters: ", np.unique(labels).size)
@@ -40,24 +40,22 @@ def display_all_patches(img, labels):
     Displays clusters on a plot.
 
     :param img: An RGB image
-    :param K: the number of clusters required
-    :returns: A matrix of the same height and width as img. Each element is one of K labels
+    :param labels: matrix of same size as img, densely labelled.
     """
     from matplotlib import pyplot as plt
-    import matplotlib.patches as mpatch
     plt.figure(figsize=(5, 5))
     for l in range(np.max(labels) + 1):
-        color = np.mean(img[labels == l], axis = 0) / 255.
-        segmentIndices =  np.where(labels == l)
-        plt.scatter(segmentIndices[1],
-                    segmentIndices[0],
-                    color = color,
-                    alpha = 0.5,
-                    marker = ",",
-                    s = 5)
+        color = np.mean(img[labels == l], axis=0) / 255.
+        segment_indices = np.where(labels == l)
+        plt.scatter(segment_indices[1],
+                    segment_indices[0],
+                    color=color,
+                    alpha=.5,
+                    marker=",",
+                    s=5)
         plt.contour(labels == l, contours=1,
-                    colors=[color], alpha = 1., linewidths = 0.1)
-    plt.imshow(img, alpha = .5)
+                    colors=[color], alpha=1., linewidths=0.1)
+    plt.imshow(img, alpha=.5)
     plt.show()
 
 
@@ -65,64 +63,61 @@ def display_one_patch(img, labels, label):
     """
     Displays cluster on a plot.
 
-    :param img: An RGB image
+    :param img: An RGB image        
+    :param labels: matrix of same size as img, densely labelled.
     :param label: the label of the patch to show
-    :returns: A matrix of the same height and width as img. Each element is one of K labels
     """
     from matplotlib import pyplot as plt
-    import matplotlib.patches as mpatch
     plt.figure(figsize=(5, 5))
-    color = np.mean(img[labels == label], axis = 0) / 255.
-    segmentIndices =  np.where(labels == label)
-    plt.scatter(segmentIndices[1],
-                segmentIndices[0],
-                color = color,
-                alpha = 0.5,
-                marker = ",",
-                s = 5)
+    color = np.mean(img[labels == label], axis=0) / 255.
+    segment_indices = np.where(labels == label)
+    plt.scatter(segment_indices[1],
+                segment_indices[0],
+                color=color,
+                alpha=0.5,
+                marker=",",
+                s=5)
     plt.contour(labels == label, contours=2,
-                colors=[color], alpha = 1., linewidths = 0.25)
-    plt.imshow(img, alpha = .5)
+                colors=[color], alpha=1., linewidths=0.25)
+    plt.imshow(img, alpha=.5)
     plt.show()
 
 
-def get_segmented_image(name, K):
+def get_segmented_image(name, k):
     """
     Gets a segmented image from file if the file exists. If not, segments the image
     and writes a segment file for next time.
     
     :param name: The name of the image file to segment, without suffix. Image file must exist. 
-    :param K: number of segments desired
+    :param k: number of segments desired
     :returns: A dictionary with the data from a segmented image
     """
-    import os.path
-    from scipy import ndimage, misc
-    img = ndimage.imread(name + '.jpg')
+    img = file_handling.get_image("small_butterfly")
     if file_handling.project_exists(name):
         print "opening project"
         segment_data = file_handling.read_segment_file(name)
         pix = segment_data["pixels"]
         labels = segment_data["labels"]
-        if K != (np.max(labels) + 1) or img.shape != pix.shape:
+        if k != (np.max(labels) + 1) or img.shape != pix.shape:
             print "segment file parameters don't match, remaking."
-            print "K:",K, np.max(labels) + 1
-            print "shape:",img.shape, pix.shape
-            os.remove(segmented_dir + name + '.segmented')
+            print "K:", k, np.max(labels) + 1
+            print "shape:", img.shape, pix.shape
+            file_handling.remove_segment_file(name)
         else:
             return segment_data
-    labels = segment(img, K)
+    labels = segment(img, k)
     segment_data = init_project_dict(img, labels)
     file_handling.write_segment_file(name, segment_data)
-    return segment_data           
+    return segment_data
 
 
 def init_project_dict(pixels, labels):
     segment_data = {
-        "pixels" : pixels,
-        "labels" : labels
-        }
+        "pixels": pixels,
+        "labels": labels
+    }
     max_label = np.max(labels)
-    hist = np.histogram(labels, bins = max_label)
+    hist = np.histogram(labels, bins=max_label)
     order = np.flip(np.argsort(hist[0]), 0)
     for i in order:
         key = "patch_%i" % i
@@ -130,7 +125,8 @@ def init_project_dict(pixels, labels):
         segment_data[key]["size"] = hist[0][i]
         segment_data[key]["visits"] = {}
     return segment_data
-        
+
+
 if __name__ == "__main__":
     segData = get_segmented_image('small_butterfly', 50)
     display_all_patches(segData["pixels"], segData["labels"])
