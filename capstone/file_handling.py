798931dda989c8e65d41f0f5be31d61bd00bd21d
IsaacWallis <ikewallis@gmail.com>
2017-05-05 22:48:27 -0600

    Cleans up file_handling.

--- capstone/file_handling.py
+++ capstone/file_handling.py
@@ -1,8 +1,6 @@
 import os
-import pickle
-import pandas as pd
-from PIL import Image
 import numpy as np
+from PIL import Image
 
 segmented_dir = "./segmented"
 main_dir = "./progress"
@@ -10,59 +8,6 @@ image_dir = "./images"
 source_dir_name = "source"
 segment_file_name = "segment.pickle"
 progress_file_name = "progress.pickle"
-
-
-def get_project_directory(name):
-    if not os.path.exists(main_dir):
-        os.makedirs(main_dir)
-    project_path = os.path.join(main_dir, name)
-    if not os.path.exists(project_path):
-        os.makedirs(project_path)
-    return project_path
-
-
-def project_exists(name):
-    if not os.path.exists(main_dir):
-        return False
-    project_path = os.path.join(main_dir, name)
-    if not os.path.exists(project_path):
-        return False
-    segment_file = os.path.join(project_path, segment_file_name)
-    if not os.path.exists(segment_file):
-        return False
-    return True
-
-
-def write_segment_file(name, data):
-    project_path = get_project_directory(name)
-    segment_file_path = os.path.join(project_path, segment_file_name)
-    output = open(segment_file_path, 'ab+')
-    pickle.dump(data, output)
-    output.close()
-
-
-def replace_segment_file(name, data):
-    if project_exists(name):
-        remove_segment_file(name)
-    write_segment_file(name, data)
-
-
-def remove_segment_file(name):
-    project_path = get_project_directory(name)
-    segment_file_path = os.path.join(project_path, segment_file_name)
-    os.remove(segment_file_path)
-
-
-def read_segment_file(name):
-    project_path = get_project_directory(name)
-    segment_file_path = os.path.join(project_path, "segment.pickle")
-    if project_exists(name):
-        output = open(segment_file_path, 'rb')
-        data = pickle.load(output)
-        output.close()
-        return data
-    else:
-        raise IOError("Can't read project, it doesn't exist.")
 
 
 def get_target_image(name):
@@ -90,24 +35,6 @@ def get_source_indices():
             img_num_list.append(int(img_num))
 
     return img_num_list
-
-
-def get_saved_progress(project_name, patch):
-    path = os.path.join(get_project_directory(project_name), progress_file_name)
-    if os.path.exists(path):
-        progress = pd.read_csv(path)
-        return progress
-    else:
-        init = pd.Dataframe(columns=["state", "loss", "t"])
-        return init
-
-
-def add_progress(name, patch, state, loss, t):
-    path = os.path.join(get_project_directory(name), progress_file_name)
-    progress = get_saved_progress(name)
-    current_t = progress.loc[patch]["t"]
-    progress.loc[patch]["t"] = [state, loss, current_t + t]
-    progress.to_csv(path)
 
 
 if __name__ == "__main__":
